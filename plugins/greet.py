c44465320881375fa692a9a8ca3ccc71b430906c
Harshitha Kattamedhi <hk574@njit.edu>
2024-10-22 22:42:28 -0400

    added logging

--- plugins/greet.py
+++ plugins/greet.py
@@ -1,11 +1,19 @@
-from commands import Command
+"""
+GreetPlugin module.
 
+This module defines the GreetPlugin class, which is a plugin for the 
+Command framework. It provides functionality to greet users by 
+printing specified arguments along with a greeting message.
+"""
+import logging
+from commands import Command
 class GreetPlugin(Command):
     """An example plugin that provides additional functionality."""
-
     command_name = "greet"
     @staticmethod
-    def execute(arg1,arg2):
-        print(arg1,arg2)
-        print("greet")
-       
+    def execute(arg1, arg2):
+        """Execute the greet command with two arguments."""
+        logging.info(arg1, arg2)
+        logging.info("greet")
+# pylint: disable=too-few-public-methods
+# pylint: disable=arguments-differ
